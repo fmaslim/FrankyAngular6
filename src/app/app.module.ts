a10ecd6f6a1887a00ad39aa27ed733d71c325236
fmaslim <franky.maslim@yahoo.com>
2018-05-22 18:48:41 -0400

    Added demo form sku component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
@@ -9,6 +11,7 @@ import { AngularRedditComponent } from './angular-reddit/angular-reddit.componen
 import { AngularRedditArticleComponent } from './angular-reddit-article/angular-reddit-article.component';
 import { InventoryProductRowComponent } from './inventory-product-row/inventory-product-row.component';
 import { InventoryProductListComponent } from './inventory-product-list/inventory-product-list.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +19,16 @@ import { InventoryProductListComponent } from './inventory-product-list/inventor
     HelloWorldComponent,
     UserItemComponent,
     UserListComponent,
-    AngularRedditComponent,
+    AngularRedditComponent,
     AngularRedditArticleComponent,
     InventoryProductRowComponent,
-    InventoryProductListComponent
+    InventoryProductListComponent,
+    DemoFormSkuComponent
 ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
