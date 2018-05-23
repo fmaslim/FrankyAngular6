ea580e6a1b367fdb43662ca79ca622611b026bc1
fmaslim <franky.maslim@yahoo.com>
2018-05-23 15:38:01 -0400

    Added FormBuilder, FormGroup, FormControl

--- src/app/demo-form-sku/demo-form-sku.component.ts
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-form-sku',
@@ -6,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-form-sku.component.css']
 })
 export class DemoFormSkuComponent implements OnInit {
+  myForm: FormGroup;
+  sku: AbstractControl;
+  productName: string;
 
-  constructor() { }
+  constructor(fbuilder: FormBuilder) {
+    this.myForm = fbuilder.group({
+      'sku': ['abc', Validators.compose([Validators.required, this.skuValidator])],
+      'productName': ['', Validators.required]
+    });
+
+    this.sku = this.myForm.controls['sku'];
+
+    this.sku.valueChanges.subscribe((value: string) => {
+      console.log('sku changed to: ', value);
+    });
+
+    this.myForm.valueChanges.subscribe((form: any) => {
+      console.log('form changed to: ', form);
+    })
+  }
 
   ngOnInit() {
   }
 
-  onSubmit(form: any) {
-    console.log('Form onSubmit called. Value submitted: ', form.sku);
+  onSubmit(value: any) {
+    console.log('Form onSubmit called. Value submitted: ', value['sku']);
+  }
+
+  skuValidator(control: FormControl): { [s: string]: boolean } {
+    if (!control.value.match('123')) {
+      return { invalidSku: true };
+    }
   }
 }
